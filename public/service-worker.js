d0600b3d7df6564e8936eb412161dedf34cb0f90
Wesley Fernando Cabrera <wesley.cabrera@hotmail.com>
2025-11-13 19:30:09 +0000

    ok

---
+++ public/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "inusitta-pwa-v2";
+const urlsToCache = [
+    "/",
+    "/manifest.json",
+    "/css/app.css",
+    "/js/app.js"
+];
+
+// Instala o Service Worker
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Intercepta requests
+self.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Atualiza cache
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) =>
+            Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        )
+    );
+});
